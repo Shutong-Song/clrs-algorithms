a63a0be6f8d429d6ee0fe9a4f07a9390af69b091
ss <simonsong004@gmail.com>
2023-08-29 08:37:34 -0400

    add clrs chapter 2

--- clrs_chapter2.md
+++ clrs_chapter2.md
@@ -55,3 +55,81 @@ std::vector<int> binary_add(std::vector<int>& arr1, std::vector<int>& arr2)
     return res;
 }
 ```
+
+
+2.2-2 selection sort  
+```cpp
+void selectionsort(std::vector<int>& arr)
+{
+    for(int i = 0; i < arr.size(); i++)
+    {
+        int smallest_idx = i;
+        for(int j = i+1; j < arr.size(); j++)
+        {
+            if(arr[smallest_idx] > arr[j])
+                smallest_idx = j;
+        }
+        std::swap(arr[smallest_idx], arr[i]);
+    }
+}
+```
+
+
+2.2-4 How can we modify almost any algorithm to have a good best-case running time?  
+Answer: check if the input is already satisfy the output condition, for example, for sort, check if the input array is already sorted, if yes, return the results
+
+
+2.3-1 Using Figure 2.4 as a model, illustrate the operation of merge sort.  
+```cpp
+void merge_(std::vector<int>& arr, int low, int mid, int high)
+{
+    std::vector<int> left(arr.begin()+low, arr.begin()+mid);
+    std::vector<int> right(arr.begin()+mid, arr.begin()+high);
+    int i = 0, j = 0;
+    int k = low;
+    while(i < left.size() && j < right.size())
+    {
+        if(left[i] <= right[j])
+        {
+            arr[k] = left[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+    while(i < left.size())
+    {
+        arr[k] = left[i];
+        i++;
+        k++;
+    }
+    while(j < right.size())
+    {
+        arr[k] = right[j];
+        j++;
+        k++;
+    }
+}
+
+void mergesort(std::vector<int>& arr, int low, int high)
+{
+    int n = arr.size();
+    if(low < high - 1)
+    {
+        int mid = (low + high)/2;
+        mergesort(arr, low, mid);
+        mergesort(arr, mid, high);
+        merge_(arr, low, mid, high);
+    }
+}
+```
+
+2.3-3 Use mathematical induction to show that when n is an exact power of 2, the solution of the recurrence  
+$$
+T(n) = \begin{cases} 2,\text{ if n= 2}\\\\2T(n/2)+n, \text{ if n = $2^k$} \end{cases}
+$$
+is $T(n) = nlgn$
--- src/cpp/sorting.cpp
+++ src/cpp/sorting.cpp
@@ -41,11 +41,81 @@ std::vector<int> binary_add(std::vector<int>& arr1, std::vector<int>& arr2)
     return res;
 }
 
+
+//selection sort
+void selectionsort(std::vector<int>& arr)
+{
+    for(int i = 0; i < arr.size(); i++)
+    {
+        int smallest_idx = i;
+        for(int j = i+1; j < arr.size(); j++)
+        {
+            if(arr[smallest_idx] > arr[j])
+                smallest_idx = j;
+        }
+        std::swap(arr[smallest_idx], arr[i]);
+    }
+}
+
+
+//mergesort: divide and conquer
+void merge_(std::vector<int>& arr, int low, int mid, int high)
+{
+    std::vector<int> left(arr.begin()+low, arr.begin()+mid);
+    std::vector<int> right(arr.begin()+mid, arr.begin()+high);
+    int i = 0, j = 0;
+    int k = low;
+    while(i < left.size() && j < right.size())
+    {
+        if(left[i] <= right[j])
+        {
+            arr[k] = left[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+    while(i < left.size())
+    {
+        arr[k] = left[i];
+        i++;
+        k++;
+    }
+    while(j < right.size())
+    {
+        arr[k] = right[j];
+        j++;
+        k++;
+    }
+}
+
+void mergesort(std::vector<int>& arr, int low, int high)
+{
+    int n = arr.size();
+    if(low < high - 1)
+    {
+        int mid = (low + high)/2;
+        mergesort(arr, low, mid);
+        mergesort(arr, mid, high);
+        merge_(arr, low, mid, high);
+    }
+}
+
 int main()
 {
-    std::vector<int> arr1 = {1, 2, 3}, arr2 = {9, 0, 7};
-    std::vector<int> res = binary_add(arr1, arr2);
-    std::for_each(res.begin(), res.end(), [](const int x){std::cout << x << "\n";});
+    std::vector<int> arr = {19, 7, 1, 8, 2, 2, 13, 7};
+    mergesort(arr, 0, arr.size());
+    //std::vector<int> test(arr.begin()+2, arr.begin()+5); //intialize with subset of another vector like python
+    //selectionsort(arr);
+    std::for_each(arr.begin(), arr.end(), [](const int x){std::cout << x << "\n";});
+
+    //std::vector<int> arr1 = {1, 2, 3}, arr2 = {9, 0, 7};
+    //std::vector<int> res = binary_add(arr1, arr2);
+    //std::for_each(res.begin(), res.end(), [](const int x){std::cout << x << "\n";});
 
 
     //std::vector<int> arr = {19, 7, 1, 8, 2, 2, 13, 7};
